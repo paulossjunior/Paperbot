5c6a3ff1a6aba7df8a52094c0f026588d4f95fba
Paulo <paulossjunior@gmail.com>
2020-11-27 07:22:00 -0300

    Update yellowpages.py

--- yellowpages.py
+++ yellowpages.py
@@ -40,44 +40,6 @@ class YellowPagesCommands(commands.Cog):
     
 
 
-    @commands.command(help='retrive all papers by a word in abstract')
-    async def find_by_word(self, ctx, arg):
-        notFound = True
-        with open('yellowpages.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=';',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for row in csv_reader:
-                abstract = row[0]
-                abstract = abstract.lower()
-                result = abstract.find(arg.lower())
-                if result != -1:
-                    notFound = False
-                    message = "- *"+row[1].strip()+"* - "+row[2].strip()+" - "+row[3].strip()
-                    await ctx.send(str(message))
-                
-                
-        if notFound:
-            message = "Nothing found! :("
-            await ctx.send(str(message))
-        
-        
-
-    @commands.command(help='retrive all papers by an author')
-    async def find_by_author(self, ctx, arg):
-        notFound = True
-        with open('yellowpages.csv') as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=';',quotechar='"', quoting=csv.QUOTE_MINIMAL)
-            for row in csv_reader:
-                author = row[2]
-                author = author.lower()
-                result = author.find(arg.lower())
-                if result != -1:
-                    notFound = False
-                    message = "- *"+row[1].strip()+"* - "+row[2].strip()+" - "+row[3].strip()
-                    await ctx.send(str(message))
-                
-        if notFound:
-            message = "Nothing found! :("
-            await ctx.send(str(message))        
     
 class YellowPages():
 
